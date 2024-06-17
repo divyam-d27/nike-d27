1bc89a5d3fefdd42c9ea9f613d2dd38b82df83e7
Divyam Dubey <divyamdubey2@gmail.com>
2024-06-17 23:05:24 +0530

    initial

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Services from "./Services";
+import SuperQuality from "./SuperQuality";
+import PopularProducts from "./PopularProducts";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import SpecialOffer from "./SpecialOffer";
+import Footer from "./Footer";
+
+export {
+  Hero,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+  CustomerReviews,
+  PopularProducts,
+  Footer,
+};
